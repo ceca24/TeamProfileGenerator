91e9826b214b84de4785792999a47b145696885c
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-13 22:01:15 -0500

    made adjustments to generateHTML.js file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,89 +1,112 @@
-// takes writeFileSync from the fs module
-const {writeFileSync} = require('fs')
-const Manager = require('../lib/manager')
-
-// function to generate team HTML page using the data that is passed through
-function generateHTML(res) {
-writeFileSync(`./index.html`, `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    />
-    <title>Team Profile Generator</title>
-  </head>
-  <div>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 jumbotron mb-3 bg-danger">
-          <h1 class="text-center text-black">The Team</h1>
+const generateTeams = (team) => {
+    
+  const generateManager = (manager) => {
+    return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
-      </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+  };
+
+  const generateEngineer = (engineer) => {
+    return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+        </ul>
+    </div>
+</div>
+        `;
+  };
+
+  const generateIntern = (intern) => {
+    return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+  };
+
+  const html = [];
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+      .join("")
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+      .join("")
+  );
+
+  return html.join("");
+};
+
+module.exports = (team) => {
+  return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
     </div>
     <div class="container">
-      <div class="row m-5 ">
-        <div class="row col-12 d-inline-flex justify-content-center"></div>
-  `)
-// loops through pased through array and goes through each role
-  for (const employee of res) {
-    if (employee.confirmManager === "Manager") {
-        console.log(employee)
-      writeFileSync(`./index.html`, `
-      <div class="card m-1">
-      <div class="card-header bg-info text-white">
-        <h2 class="card-title">${employee.name}</h2>
-        <h3 class="card-subtitle mb-2 text-dark">Manager</h3>
-      </div>
-  </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${employee.id}</li>
-        <li class="list-group-item"><a href = "mailto: ${employee.email}">Email: ${employee.email}</a></li>
-        <li class="list-group-item">Office Number: ${employee.officeNumber}</li>
-      </ul>
-  </div>
-  `, {flag: 'a'})
-      } 
-    else if (employee.confirmIntern === "Intern") {
-      console.log(employee)
-    writeFileSync(`./index.html`, `
-    <div class="card m-1">
-    <div class="card-header bg-info text-white">
-      <h2 class="card-title">${employee.name}</h2>
-      <h3 class="card-subtitle mb-2 text-dark">Intern</h3>
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                ${generateTeams(team)}
+            </div>
+        </div>
     </div>
-</div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">ID: ${employee.id}</li>
-      <li class="list-group-item"><a href = "mailto: ${employee.email}">Email: ${employee.email}</a></li>
-      <li class="list-group-item">School: ${employee.school}</li>
-    </ul>
-</div>
-`, {flag: 'a'})
-    } 
-    else  if (employee.confirmEngineer === "Engineer") {
-        console.log(employee)
-      writeFileSync(`./index.html`, `
-      <div class="card m-1">
-      <div class="card-header bg-info text-white">
-        <h2 class="card-title">${employee.name}</h2>
-        <h3 class="card-subtitle mb-2 text-dark">Engineer</h3>
-      </div>
-  </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${employee.id}</li>
-        <li class="list-group-item"><a href = "mailto: ${employee.email}">Email: ${employee.email}</a></li>
-        <li class="list-group-item"><a href="${employee.github}">Github: ${employee.github}</a></li>
-      </ul>
-  </div>
-  `, {flag: 'a'})
-    } 
-  }
-}
-
-// exports generateHTML function from module
-module.exports = generateHTML; 
+</body>
+</html>
+    `;
+};
