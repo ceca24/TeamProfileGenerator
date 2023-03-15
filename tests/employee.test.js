83f7a58e00ff9d31d6849e03646ba3f1ce323ae8
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-14 21:42:06 -0500

    removed all the code in every folder/file to start from scratch

--- tests/employee.test.js
+++
@@ -1,9 +0,0 @@
-const Employee = require("../lib/Employee.js");
-
-test("Verifies a employee object was created", () => {
-  const employee = new Employee("Junji Ito", 2552, "junji_ito@uzumaki.com")
-  
-  expect(employee.name).toEqual(expect.any(String));
-  expect(employee.id).toEqual(expect.any(Number));
-  expect(employee.email).toEqual(expect.any(String));
-})
